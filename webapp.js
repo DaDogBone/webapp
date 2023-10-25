cc35e38630f9de5f6a218bd45bc79938f6418604
GGGZED <ethandmaxson@gmail.com>
2023-10-25 12:11:03 -0400

    Prof Eipps fixed

--- webapp.js
+++ webapp.js
@@ -1,83 +1,79 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const path = require ('path')
-
-const app=express()
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
 
 app.engine('handlebars', expressHandlebars.engine())
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname), 'views');
-app.use(express.static(path.join(__dirname,'static')))
-const port = process.env.PORT || 3000
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'static')));
 
-const mariadb = require('mariadb');
+
+// establish a connection to a mariadb database
+const mariadb = require('mariadb')
 const pool = mariadb.createPool({
     host: 'localhost',
     user: 'week6user',
     password: 'week6pw',
     connectionLimit: 5
-});
+})
 
-app.get('/test', async (req, res) => {
+
+// route to test my database setup
+app.get('/test', async(req, res) => {
     let conn;
     try {
         conn = await pool.getConnection();
-
-        const rows = await conn.query("SELECT 1 as val");
-        console.log(dbtest);
-
-        const week6db = await conn.query('use week6db');
-        console.log(week6db + 'hello');
-
-
-
-        let rand = Math.random() * 100;
-
-        const insert_result = await conn.query("insert into t values (?)", [rand]);
-        console.log(insert_result); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-
-        let trows = await conn.query("SELECT x from t as val_x");
-        console.log(rows); //[ {x: 1}, meta: ... ]
-
-        res.render('showdata', {
-            title: 'data results',
-            data: rows,
-            isDeleted: false
-        })
-
-    } catch (err) {
+        const dbtest = await conn.query('select 1 as val')
+        console.log(dbtest)
+       
+        res.type('text/plain')
+        res.status(200)
+        res.send('made it to route: /test')
+    } catch(err) {
         console.log(err)
-        throw err;
     } finally {
-        console.log('what just happened')
         if (conn) return conn.end();
     }
 })
 
-
-app.get('/', (req, res) => {
+// route to /
+app.get('/', (req, res) => {    
     res.render('home', {
-        title: 'Artdrop',
-        name: 'Me'
+        title: 'TGIFF Home',
+        name: 'Professor Eipp',
     })
 })
 
+
+// route to /about
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About ArtDrop',
-        
+        title: 'Meadowlark About',
     })
 })
 
-app.use((req, res, next) => {
-    res.status(404)
-    res.render('404')
-})
-/*
+// custom 500
 app.use((err, req, res, next) => {
     console.error(err.message)
+    res.type('text/plain')
     res.status(500)
-    res.render('500')
+    res.send('500 - Server Error')
 })
-*/
-app.listen(port, () => console.log(`step on http://localhost:${port} Press Ctrl-C to terminate.`))
+
+
+// custom 404
+app.use((req, res)=> {
+    res.type('text/plain')
+    //console.log(res.get('Content-Type'))
+    res.status(404)
+    res.send('404 - Page Not Found')
+})
+
+
+// start the server listening for requests...
+app.listen(port, () => {
+    console.log(`Running on http://localhost:${port} ` +
+    `Press Ctrl-C to terminate.`)
+})
