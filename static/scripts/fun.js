c9b9179f362c774f87de8d6ff1dce0a9083739da
GGGZED <ethandmaxson@gmail.com>
2023-11-03 11:17:31 -0400

    made some fun

---
+++ static/scripts/fun.js
@@ -0,0 +1,8 @@
+function fun(){
+    console.log('made it into fun')
+    document.getElementById("result").innerHTML = "fun"
+}
+
+function nofun(){
+    document.getElementById("result").innerHTML = "no fun at all!"
+}
