f0cd3d385a0d12d7f20e148438bf72a2dfa7bdb3
GGGZED <ethandmaxson@gmail.com>
2023-11-27 11:29:03 -0500

    sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,9 +1,16 @@
-use dbweek6;
+/* login to mariadb using: sudo mysql */
+/* source this script using: source init.sql */
+/* source your reset file: source reset-data.sql */
 
-create or replace table week6_data (
+DROP DATABASE IF EXISTS week6db;
+CREATE DATABASE 'week6db';
 
-);
+DROP USER IF EXISTS 'week6user'@localhost;
+CREATE USER 'week6user'@localhost IDENTIFIED BY 'week6pw';
 
-create or replace table week6_accounts(
+/* SELECT User FROM mysql.user WHERE User = 'week6user'; */
 
-);
+GRANT ALL PRIVILEGES ON 'week6db'.* TO 'week6user'@localhost IDENTIFIED BY 'week6pw';
+FLUSH PRIVILEGES;
+
+SHOW GRANTS FOR 'week6user'@localhost;
