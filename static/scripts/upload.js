d3c3596796c2dcec50fab45a62a7de999ab6fc3f
DaDogBone <ethandmaxson@gmail.com>
2023-12-15 04:05:51 -0500

    postimages scrpt

---
+++ static/scripts/upload.js
@@ -0,0 +1,36 @@
+const images = [];
+
+function imgUpload() {
+    const input = document.getElementById('userImage');
+
+    if (input.files.length > 0) {
+
+        const file = input.files[0];
+
+        const reader = new FileReader();
+        ReadableStream.onload = function (e) {
+            imageArray.push(e.target.result);
+
+            displayImages();
+        };
+        reader.readAsDataURL(file);
+
+    } else {
+        alert('please select an image');
+    }
+
+}
+
+function displayImages() {
+
+    const imageContainer = document.getElementById('imageContainer')
+
+    imageContainer.innerHTML = '';
+
+    imageArray.forEach(dataURL => {
+        const imgElement = document.createElement('img');
+        imgElement.src = dataURL;
+        imageContainer.appendChild(imgElement);
+    });
+
+}
